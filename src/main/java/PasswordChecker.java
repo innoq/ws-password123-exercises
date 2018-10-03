5572cca25d4f3896df3b2cddd9bf044c7af1f86e
Christoph Iserlohn <christoph.iserlohn@innoq.com>
2018-10-03 12:18:05 +0200

    Initial commit

---
+++ src/main/java/PasswordChecker.java
@@ -0,0 +1,13 @@
+public class PasswordChecker {
+
+    private String url = "https://api.pwnedpasswords.com/range/";
+
+    public boolean isPwnd(char[] password) {
+        /*
+          Use the PwndPasswords-API (see https://haveibeenpwned.com/API/v2#PwnedPasswords)
+          to check if 'password' has previously been exposed in a data breach. Return 'true'
+          if 'password' has been exposed, otherwise return 'false'.
+          Clear all sensitive date before the method returns.
+        */
+    }
+}
