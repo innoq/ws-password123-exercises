299ace8772bdad20279d8e3a3545cc90afc69592
Jochen Christ <jochen.christ@innoq.com>
2018-10-05 17:09:36 +0200

    TOTP exercise solution

--- src/test/java/TotpTest.java
+++ src/test/java/TotpTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.assertTrue;
 
 import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,10 +13,9 @@ public class TotpTest {
     // You can use the GoogleAuthenticator from the https://github.com/wstrange/GoogleAuth implementation
     GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator();
 
-    // TODO Generate a shared secret (which must be saved with the login)
-    String sharedSecret = "";
-    // TODO Generate a URL, which returns the QR code, that can be scanned by the user.
-    String url = "";
+    GoogleAuthenticatorKey credentials = googleAuthenticator.createCredentials();
+    String sharedSecret = credentials.getKey();
+    String url = GoogleAuthenticatorQRGenerator.getOtpAuthURL("jochenchrist.de", "jochen", credentials);
 
     // Don't modify the outputs and asserts.
     System.out.println("Shared Secret: " + sharedSecret);
@@ -25,10 +26,8 @@ public class TotpTest {
 
   @Test
   public void verifyToken() {
-    // TODO Enter the shared secret from your scanned QR code
-    String sharedSecret = "ENTER_YOUR_SHARED_SECRET_HERE";
-    // TODO Enter the current token, displayed in your app.
-    int token = 123456;
+    String sharedSecret = "4NKMW6B2IXCP7RIL";
+    int token = 334441; // The token is valid for 30 seconds
 
     boolean isValid = new GoogleAuthenticator().authorize(sharedSecret, token);
 
